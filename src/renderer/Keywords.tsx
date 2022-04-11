20edee603b314ec7cb03fc6eb1a616772dc6c57f
PhilippeMts <phi.metais@gmail.com>
2022-04-11 18:55:31 +0200

    use React state on Keywords

--- src/renderer/Keywords.tsx
+++ src/renderer/Keywords.tsx
@@ -1,50 +1,58 @@
-import styles from './Keywords.module.scss';
+import styles from "./Keywords.module.scss";
+import { useState } from "react";
+import { functions } from "electron-log";
 
-const KeywordCard = ({ keyword }: { keyword: string }) => {
-  const onRemoveClick = () => {
-    const keywords = window.electron.store.get('keywords') || [];
-    // remove element from array
-    const newKeywords = keywords.filter((k: string) => k !== keyword);
-    window.electron.store.set('keywords', newKeywords);
-  };
-
+const KeywordCard = ({ keyword, onRemove }: { keyword: string }) => {
   return (
     <div className={styles.keywordCard}>
       <h3>{keyword}</h3>
       <span>link to fragments</span>
-      <button onClick={onRemoveClick}>remove</button>
+      <button onClick={onRemove}>remove</button>
       {/* move to dedicated page */}
     </div>
   );
 };
 
-const KeywordCreationCard = () => {
-  const onCreationClick = () => {
-    // TODO do not forget to normalize the new keyword
-    const newKeyword = new Date().getTime();
-    const keywords = window.electron.store.get('keywords') || [];
-    if (!keywords.includes(newKeyword)) {
-      // append new keyword
-      keywords.push(newKeyword);
-      window.electron.store.set('keywords', keywords);
-    }
-  };
-
+const KeywordCreationCard = ({ onAdd }) => {
   return (
-    <button className={styles.fragmentCard} onClick={onCreationClick}>
+    <button className={styles.fragmentCard} onClick={onAdd}>
       <h3>Add ➕</h3>
     </button>
   );
 };
 
 const Keywords = () => {
-  const keywords = window.electron.store.get('keywords') || []; // TODO use reducer
+  const initialKeywords = window.electron.store.get("keywords") || [];
+  const [keywords, setKeywords] = useState(initialKeywords);
+
+  const addKeyword = (keyword: string) => {
+    // TODO do not forget to normalize the new keyword
+    // add to list if not already present
+    if (!keywords.includes(keyword)) {
+      const newKeywords = [...keywords, keyword];
+      setKeywords(newKeywords);
+      window.electron.store.set("keywords", newKeywords);
+    }
+  };
+
+  const removeKeyword = (keyword: string) => {
+    // remove from list
+    const newKeywords = keywords.filter((k: string) => k !== keyword);
+    setKeywords(newKeywords);
+    window.electron.store.set("keywords", newKeywords);
+  };
+
   return (
     <div className={styles.vWrapper}>
       {keywords.map((kw: string) => {
-        return <KeywordCard keyword={kw} key={kw} />;
-      })}
-      <KeywordCreationCard />
+        return <KeywordCard keyword={kw} key={kw} onRemove={() => {
+          removeKeyword(kw);
+        }} />;
+      })
+      }
+      <KeywordCreationCard onAdd={() => {
+        addKeyword(new Date().getTime().toString());
+      }} />
     </div>
   );
 };
