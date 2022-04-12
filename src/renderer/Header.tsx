a0e561f3d9f85c14d4a7af2a662a17f220af579e
PhilippeMts <phi.metais@gmail.com>
2022-04-12 16:38:34 +0200

    remove broken header navigation feature

--- src/renderer/Header.tsx
+++ src/renderer/Header.tsx
@@ -1,18 +1,12 @@
 import styles from './Header.module.scss';
-import { Link, useLocation } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const Header = () => {
-  const pathname = useLocation().pathname;
   return (
     <div>
       <Link to={'/'}>
         Home
       </Link>
-      {pathname !== '/' && (
-        <Link to={'..'}>
-          🔙
-        </Link>
-      )}
     </div>
   );
 };
